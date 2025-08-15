3da29c8b7a9e0e7155f819a3f23f744f7f5f2dc9
DanhCypher <brucelee6788@gmail.com>
2025-08-15 18:21:21 +0700

    update html, css and app.js

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
-app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
-app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'views', 'contact.html')));
+// Routes for the HTML pages
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'about.html')));
+app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'contact.html')));
 
+// Start the server
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
